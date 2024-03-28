7452f4001d1b91348acb488c9150fc3d325bc130
PapaParkii <ptown01au@gmail.com>
2024-03-28 12:01:11 -0600

    product

--- SportsPro/Controllers/HomeController.cs
+++ SportsPro/Controllers/HomeController.cs
@@ -1,22 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Linq;
 
 namespace SportsPro.Controllers
 {
     public class HomeController : Controller
     {
+
+        private SportsProContext context { get; set; }
+
+        public HomeController(SportsProContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+
+        [Route("About")]
         public IActionResult About()
         {
             return View();
         }
 
-        public IActionResult Product()
-        {
-            return View();
-        }
+
     }
 }
